5ce01ccc495edd7f9da76698df8037b481f6090d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-19 12:05:31 +0100

    ft_printf.h : implemented t_format

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 10:37:28 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/19 11:59:33 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/19 12:05:01 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
+
+/* Struct to track format parameters */
+typedef struct s_format
+{
+	char	specifier;
+	int		minus;
+	int		plus;
+	int		width;
+	int		precision;
+	int		neg_precision;
+	int		zero;
+	int		dot;
+	int		space;
+	int		sharp;
+}	t_format;
 
 #endif
