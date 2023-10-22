19a28c1bd412f9c17a1b8bff3e7cc59876b240c0
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-22 16:41:34 +0100

    ft_print_chars.c : parsed to prsd

--- ft_print_chars.c
+++ ft_print_chars.c
@@ -6,56 +6,56 @@
 /*   By: zedr0 <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 17:16:29 by zedr0             #+#    #+#             */
-/*   Updated: 2023/10/22 12:18:41 by zedr0            ###   ########.fr       */
+/*   Updated: 2023/10/22 16:40:49 by zedr0            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_char(t_format parsed, va_list ap)
+int	ft_print_char(t_format prsd, va_list ap)
 {
 	char	c;
 	int		len;
 	int		n;
 
 	len = 0;
-	n = (parsed.width - parsed.precision);
-	if (parsed.specifier == 'c')
+	n = (prsd.width - prsd.precision);
+	if (prsd.specifier == 'c')
 		c = va_arg(ap, int);
 	else
 		c = '%';
-	parsed.precision = 0;
-	if (!parsed.minus && parsed.zero)
+	prsd.precision = 0;
+	if (!prsd.minus && prsd.zero)
 		len += ft_putnchar_fd('0', 1, n);
-	else if (!parsed.minus && (parsed.width > parsed.precision))
+	else if (!prsd.minus && (prsd.width > prsd.precision))
 		len += ft_putnchar_fd(' ', 1, n);
 	len += ft_putchar_fd(c, 1);
-	if (parsed.minus && (n > 0))
+	if (prsd.minus && (n > 0))
 		len += ft_putnchar_fd(' ', 1, n);
 	return (len);
 }
 
-int	ft_print_str(t_format parsed, va_list ap)
+int	ft_print_str(t_format prsd, va_list ap)
 {
 	char	*str;
 	int		len;
 	int		n;
 
 	len = 0;
-	n = (parsed.width - parsed.precision);
+	n = (prsd.width - prsd.precision);
 	str = va_arg(ap, char *);
 	if (!str)
 		str = "(null)";
-	if (parsed.dot || (parsed.precision > (int)ft_strlen(str))
-		|| parsed.precision < 0)
-		parsed.precision = (int)ft_strlen(str);
-	if (!parsed.minus && (parsed.width > parsed.precision) && parsed.zero
-		&& (!parsed.dot || parsed.neg_precision))
+	if (prsd.dot || (prsd.precision > (int)ft_strlen(str))
+		|| prsd.precision < 0)
+		prsd.precision = (int)ft_strlen(str);
+	if (!prsd.minus && (prsd.width > prsd.precision) && prsd.zero
+		&& (!prsd.dot || prsd.neg_precision))
 		len += ft_putnchar_fd('0', 1, n);
-	else if (!parsed.minus && ((parsed.width - parsed.precision) > 0))
+	else if (!prsd.minus && ((prsd.width - prsd.precision) > 0))
 		len += ft_putnchar_fd(' ', 1, n);
-	len += ft_putstrn_fd(str, 1, parsed.precision);
-	if (parsed.minus && (parsed.width - parsed.precision > 0))
+	len += ft_putstrn_fd(str, 1, prsd.precision);
+	if (prsd.minus && (prsd.width - prsd.precision > 0))
 		len += ft_putnchar_fd(' ', 1, n);
 	return (len);
 }
