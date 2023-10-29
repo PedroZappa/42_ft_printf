a30fcc39fb3af0e41c82b485fbf9f544ea2d9675
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-10-29 20:16:54 +0000

    Commented out momentarily unnused code

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 19:33:44 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/29 19:26:55 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/29 20:12:35 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 #include "libft/libft.h"
 
 // p = parsed
-static char	*ft_sharp(t_format p);
+// static char	*ft_sharp(t_format p);
 
-int	ft_print_x(t_format p, va_list ap)
-{
+// int	ft_print_x(t_format p, va_list ap)
+// {
+//
+// }
+//
+// int	ft_print_ptr(t_format p, va_list ap)
+// {
+//
+// }
 
-}
-
-int	ft_print_ptr(t_format p, va_list ap)
-{
-
-}
-
-static char	*ft_sharp(t_format p)
-{
-	if (p.specifier == 'X')
-		return ("0X");
-	return ("0x");
-}
+// static char	*ft_sharp(t_format p)
+// {
+// 	if (p.specifier == 'X')
+// 		return ("0X");
+// 	return ("0x");
+// }
--- ft_print_nbrs.c
+++ ft_print_nbrs.c
@@ -6,22 +6,22 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 19:33:30 by passunca          #+#    #+#             */
-/*   Updated: 2023/10/29 19:26:37 by passunca         ###   ########.fr       */
+/*   Updated: 2023/10/29 20:12:49 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static char	ft_sign(t_format prsd);
+// static char	ft_sign(t_format prsd);
 
-int	ft_print_diu(t_format prsd, va_list ap)
-{
+// int	ft_print_diu(t_format prsd, va_list ap)
+// {
+//
+// }
 
-}
-
-static char	ft_sign(t_format prsd)
-{
-	if (prsd.plus)
-		return ('+');
-	return ('-');
-}
+// static char	ft_sign(t_format prsd)
+// {
+// 	if (prsd.plus)
+// 		return ('+');
+// 	return ('-');
+// }
